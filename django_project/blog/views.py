b41a51e2b96fe68fb9ebc4f3b54cbe8c435a14a3
frank400 <j.victorhuguenin2018@gmail.com>
2020-01-21 16:23:43 -0300

    update view.py to use mysql db

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,24 +1,13 @@
 from django.shortcuts import render
-
-posts=[
-	{'author':'alex',
-	'date':'12/2/2020',
-	'content':'first post',
-	'title':'Brazil'
-	},
-	{'author':'marie',
-	'date':'12/3/2020',
-	'content':'second post',
-	'title':'fuck you'
-	}
-]
+from .models import Post
 
 
 def home(request):
-	context={
-		'posts':posts
-	}
-	return render(request, 'blog/home.html',context)
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
-	return render(request, 'blog/about.html',{'title':'About'})
+    return render(request, 'blog/about.html', {'title': 'About'})

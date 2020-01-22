44d30ff82b16af73e2959b7630bf8777f633c8a4
frank400 <j.victorhuguenin2018@gmail.com>
2020-01-22 18:11:54 -0300

    insertUsers apps to installed_apps

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['blog/templates/blog','users/templates/users'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

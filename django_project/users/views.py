33130c202e0aaaecbe936377f70cc273f2cb7213
frank400 <j.victorhuguenin2018@gmail.com>
2020-01-22 23:55:33 -0300

    set a template to profile page and turn it exclusive to logged users

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -9,9 +10,14 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! Now you can log in')
+            messages.success(
+                request, f'Your account has been created! Now you can log in')
             return redirect('login')
-        
+
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
